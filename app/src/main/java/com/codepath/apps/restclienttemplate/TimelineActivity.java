bfadea85fe7ec946664c147bcffa42d63ce4f392
Emmanuel Onung <emyonung@fb.com>
2022-06-08 16:20:16 -0700

    Added a Character Counting feature

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -66,8 +66,9 @@ public class TimelineActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item){
         if (item.getItemId() == R.id.compose) {
             // compose item has been clicked
-            Toast.makeText(this, "Compose!", Toast.LENGTH_SHORT).show();
             // navigate to the compose activity
+            Intent intent = new Intent(this, ComposeActivity.class);
+            startActivity(intent);
             return true;
         }
         return super.onOptionsItemSelected(item);
